217e1b62e0b33dcd7f02adedf603a49150cd9cc2
Alex <cheny737@gmail.com>
2023-07-20 16:26:11 -0400

    thursday commit

--- script.js
+++ script.js
@@ -1,95 +1,137 @@
-const gridContainer = document.querySelector(".grid");
+const section = document.querySelector("section");
+const playerLivesCount = document.querySelector("span");
+let playerLives = 10;
+const button = document.querySelector("button");
 
-//card options
- const cardArray=[
-{name: 'a',
-img: 'a.png'},
-{name: 'b',
-img: 'b.png'},
-{name: 'c',
-img: 'c.png'},
-{name: 'd',
-img: 'd.png'},
-{name: 'e',
-img: 'e.png'},
-{name: 'f',
-img: 'f.png'},
-{name: 'g',
-img: 'g.png'},
-{name: 'h',
-img: 'h.png'},
- {name: 'a',
-img: 'a.png'},
-{name: 'b',
-img: 'b.png'},
-{name: 'c',
-img: 'c.png'},
-{name: 'd',
-img: 'd.png'},
-{name: 'e',
-img: 'e.png'},
-{name: 'f',
-img: 'f.png'},
-{name: 'g',
-img: 'g.png'},
-{name: 'h',
-img: 'h.png'},
+button.addEventListener("click", function (e) {
+  restart("Restarting!");
+});
 
- ]
+//link text
+playerLivesCount.textContent = playerLives;
 
- const grid= document.querySelector('.grid');
- var cardsChosen=[];
- var cardsChosenId=[];
- var cardsWon=[]
+//we generate card options
+const getData = () => [
+  { name: "a", imgSrc: "a.png" },
+  { name: "b", imgSrc: "b.png" },
+  { name: "c", imgSrc: "c.png" },
+  { name: "d", imgSrc: "d.png" },
+  { name: "e", imgSrc: "e.png" },
+  { name: "f", imgSrc: "f.png" },
+  { name: "g", imgSrc: "g.png" },
+  { name: "h", imgSrc: "h.png" },
+  { name: "a", imgSrc: "a.png" },
+  { name: "b", imgSrc: "b.png" },
+  { name: "c", imgSrc: "c.png" },
+  { name: "d", imgSrc: "d.png" },
+  { name: "e", imgSrc: "e.png" },
+  { name: "f", imgSrc: "f.png" },
+  { name: "g", imgSrc: "g.png" },
+  { name: "h", imgSrc: "h.png" },
+];
 
+// getData is stored is Data
+// const data =getData();
 
+// randomize
+const randomize = () => {
+  const cardData = getData();
+  cardData.sort(() => Math.random() - 0.5); //randomizes the data
+  return cardData; //returns the new randomized data
+};
 
-//creating the board
-function creatBoard(){
-    for(let i=0; i<cardArray.length; i++){
-var card = document.createElement ('img')
-card.setAttribute('src', 'blank.png')
-card.setAttribute('data-id', i)
-card.addEventListener('click',flipcard)
-grid.appendChild(card)
-    }
-}
-//shorting the cards 
-cardArray.sort(()=>.5-Math.random())
+//Card Generator Function
+const cardGenerator = () => {
+  const cardData = randomize();
 
+  //Generate the HTML
+  cardData.forEach((item) => {
+    const card = document.createElement("div");
+    const face = document.createElement("img");
+    const back = document.createElement("div");
+    back.classList = "back";
+    card.classList = "card";
+    face.classList = "face";
 
+    //attach the info/image to the cards
 
-//click on to match the card
-function checkForMatch(){
-    var cards=document.querySelectorAll('img')
-    const optionOneId=cardsChosenId[0]
-    const optionTwoId=cardsChosenId[2]
-    if(cardsChosenId===cardsChosenId[2]){
-        alert ('you found a math')
-        cards[optionOneId].setAttribute('src', 'optionOneId')
-        cards[optionTwoId].setAttribute('src', 'optionTwoID')
-        cardsWon.push(cardsChosen)
-    }else{
+    face.src = item.imgSrc;
+    card.setAttribute("name", item.name); // attach name to the card
 
-        //flip the cards over to play agian
-        cards[optionOneId].setAttribute('src', 'blank.png')
-        cards[optionTwoId].setAttribute('src', 'blank.png')
-        alert ('try again!')
-    }
+    //attach the cards to the section
+    section.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back);
+
+    card.addEventListener("click", handleClick);
+  });
+};
+
+function handleClick(e) {
+  this.classList.toggle("toggleCard");
+  checkCards(e);
 }
 
-//flip your card
-function flipcard(){
-    var cardID= this.getAttribute('data-id')
-    cardsChosenId.push(cardArray[cardID].name)
-    cardsChosen.push(cardID)
-    this.setAttribute('src', cardArray[cardID].img)
-    if(cardsChosen.length===2){
-        setTimeout(checkForMatch, 500)
+// check cards
+const checkCards = (e) => {
+  const clickedCard = e.target; //when we click on the target
+  const toggleCard = document.querySelectorAll(".toggleCard");
+  const flippedCards = document.querySelectorAll(".flipped");
+  clickedCard.classList.add("flipped"); //adding flipped to clickedcard
+  console.log(flippedCards);
+
+  console.log(clickedCard);
+
+  //logic
+  if (flippedCards.length === 2) {
+    if (
+      flippedCards[0].getAttribute("name") ===
+      flippedCards[1].getAttribute("name")
+    ) {
+      console.log("Matched!");
+      flippedCards.forEach((card) => {
+        card.classList.add("disable"); //disable the clicker
+        card.classList.remove('flipped');
+        // once it's fipped and matched, we cant click on it anymore
+        // card.style.pointerEvents='none';
+        // console.log(flippedCards[0]);
+      });
+      
+    } else {
+      console.log("Try Again!");
+      flippedCards.forEach((card) => {
+        card.classList.remove("flipped");
+        // card.classList.remove("toggleCard");
+        setTimeout(() => card.classList.remove("toggleCard"));
+      });
+      playerLives--;
+      playerLivesCount.textContent = playerLives;
+      if (playerLives === 0) {
+        restart("you LOST! Try Again!");
+      }
     }
+  }
+  //Check to see if you won the Game
+  if (toggleCard.length === 16) {
+    restart("YOU WON!");
+  }
+};
+//restart
+const restart = (text) => {
+  let cardData = randomize();
+  let faces = document.querySelectorAll(".face");
+  let cards = document.querySelectorAll(".card");
+  cardData.forEach((item, index) => {
+    cards[index].classList.remove("toggleCard");
+    //randomize once it's lost
 
-}
+    cards[index].style.pointerEvents = "all"; //add pointevent back
+    faces[index].src = item.imgSrc; //change image
+    cards[index].setAttribute("name", item.name); //add attribute
+  });
+  playerLives = 10;
+  playerLivesCount.textContent = playerLives;
+  setTimeout(() => window.alert(text), 100);
+};
 
-
-
-creatBoard()
+cardGenerator();
